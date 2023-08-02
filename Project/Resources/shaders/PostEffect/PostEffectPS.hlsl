1162c1ed6bbe11ed43be449a1cfda0f3f46e01c9
simizu <94036084+simizuzu@users.noreply.github.com>
2023-08-03 03:14:46 +0900

    平均ぼかしと色反転までできた

--- Project/Resources/shaders/PostEffect/PostEffectPS.hlsl
+++ Project/Resources/shaders/PostEffect/PostEffectPS.hlsl
@@ -6,14 +6,28 @@ SamplerState smp : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-    float4 colortex0 = tex0.Sample(smp, input.uv);
-    float4 colortex1 = tex1.Sample(smp, input.uv);
+    float blurLevel = 4.0f;
+    float vForRange = 5.0f;
+    float uForRange = 5.0f;
     
-    float4 color = colortex0;
-    if (fmod(input.uv.y, 0.1f) < 0.05f)
+    //•½‹Ïƒuƒ‰[
     {
-        color = colortex1;
-    }
+        const float offsetU = blurLevel / 1280;
+        const float offsetV = blurLevel / 720;
+        float4 color = { 0, 0, 0, 0 };
+        
+        for (float v = 0; v < vForRange; v++)
+        {
+            for (float u = 0; u < uForRange; u++)
+            {
+                float2 offSet = float2((u - 1) * offsetU, (v - 1) * offsetV);
+                color += tex0.Sample(smp, saturate(input.uv - offSet));
+            }
+        }
 
-    return float4(color.rgb, 1);
+        float4 texcolor = color / (uForRange * vForRange);
+        return float4(1.0f - texcolor.rgb, 1);
+    }
+    
+
 }
